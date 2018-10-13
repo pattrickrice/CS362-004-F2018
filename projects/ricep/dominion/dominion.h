1f1d54acab469b63a67665e2d38c9d525a32222b
pattrickrice <patrickrice20@gmail.com>
2018-10-12 21:22:09 -0700

    Refactor adventurer to it's own function

--- projects/ricep/dominion/dominion.h
+++ projects/ricep/dominion/dominion.h
@@ -128,4 +128,11 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+int card_adventurer(
+		int drawntreasure,
+		struct gameState *state,
+		int currentPlayer,
+		int *cardDrawn,
+		int *temphand,
+		int *z);
 #endif
