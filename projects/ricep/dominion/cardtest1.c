b88c702a78e15162181e94e1983724a2f9c216bd
pattrickrice <Patrickrice20@gmail.com>
2018-10-28 16:20:17 -0700

    save CMAKELists file before rebase

--- projects/ricep/dominion/cardtest1.c
+++ projects/ricep/dominion/cardtest1.c
@@ -1,4 +1,23 @@
-//
-// Created by Patrick on 10/28/2018.
-//
+#include "utils.h"
+#include "dominion.h"
 
+int testPlayerTurn();
+
+/******************************************************************************
+ * UnitTest for the smithy card function
+ *****************************************************************************/
+int main(){
+    logV("[Starting cardtest1] - Testing the smithy card in Dominion.c");
+    int testsPassed = testPlayerTurn();
+    logV("[cardtest1 RESULTS] ---------------------------------------------");
+    printResults(1, testsPassed);
+    logV("[Ending cardtest1] - Testing the smithy cardin Dominion.c\n\n");
+}
+
+/******************************************************************************
+ * Loop though maximum recommended number of players for the game, and check
+ * that the function returns that it is their turn.
+ *****************************************************************************/
+int testPlayerTurn(){
+    return 0;
+}
